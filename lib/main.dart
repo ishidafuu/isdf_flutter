97f3b384a0d0318de9861825a3bbb8bffd5f7cb7
ishidafuu <ishidafuu@gmail.com>
2020-04-05 11:38:28 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class FirstPage extends StatelessWidget{
               client.close();
             }
           },
-          child: Text('http'),
+          child: Text('http2'),
         ),
       ),
     );
